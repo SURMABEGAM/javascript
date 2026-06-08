a9741a382438a46a37087e2cbdff34d91b0ff7fd
Surma Begam <sbtisha2001@gmail.com>
2026-06-08 16:09:51 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+var name = "World";
+console.log(name);
+console.log(typeof name);
+var age = 30;
+console.log(age);
+console.log(typeof age);
+
+var isStudent = true;
+console.log(isStudent);
+console.log(typeof isStudent);
+
+var hobbies = ["Reading", "Traveling", "Cooking"];
+console.log(hobbies);
+console.log(typeof hobbies);
+
+var person = {
+  name: "Alice",
+  age: 25,
+};
+console.log(person);
+console.log(typeof person);
+
+// Undefined variable
+var undefinedVariable;
+console.log(undefinedVariable);
+console.log(typeof undefinedVariable);
+
+// Null variable
+var sohel = null;
+console.log(sohel);
+console.log(typeof sohel);
+
+// Symbol variable
+var uniqueId = Symbol("id");
+console.log(uniqueId);
+console.log(typeof uniqueId);
+
+// BigInt variable
+var bigNumber = 123456789012345678901234567890n;
+console.log(bigNumber);
+console.log(typeof bigNumber);
+// parseInt and parseFloat
+var num1 = "42";
+var num2 = "3.14";
+console.log(parseInt(num1));
+console.log(parseFloat(num2));
+
+// isNaN and isFinite
+var value1 = "abc";
+var value2 = 10;
+console.log(isNaN(value1));
+console.log(isFinite(value2));
+// toString and valueOf
+var num = 123;
+console.log(num.toString());
+console.log(num.valueOf());
+console.log(typeof num);
+
+// String concatenation
+var greeting = "Hello, " + name + "!";
+console.log(greeting);
+console.log(typeof greeting);
---
+++ math.js
@@ -0,0 +1,16 @@
+var num = 0;
+var result = num * num;
+//console.log(result);
+
+var a = 0;
+var b = 0;
+var sum = a - b * result;
+console.log(sum);
+
+if (sum > 0) {
+  console.log("The result is positive.");
+} else if (sum < 0) {
+  console.log("The result is negative.");
+} else {
+  console.log("The result is zero.");
+}
