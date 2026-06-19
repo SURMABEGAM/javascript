72c2b933b8d8e5dc8ecdc524db79e5931f1a046a
Surma Begam <sbtisha2001@gmail.com>
2026-06-20 02:40:36 +0600

    loop

---
+++ loop.js
@@ -0,0 +1,25 @@
+for (i = 0; i < 14; i++) {
+  //console.log(i);
+  if (i >= 10) {
+    // console.log("The loop has completed.");
+  }
+}
+
+let n = 20;
+while (n < 51) {
+  if (n > 30) {
+    break;
+  }
+  //console.log(n);
+  n++;
+}
+
+let m = 10;
+while (m > 0) {
+  //
+  if (m <= 1) {
+    break;
+  }
+  console.log(m);
+  m--;
+}
--- math.js
+++ math.js
@@ -5,12 +5,36 @@ var result = num * num;
 var a = 0;
 var b = 0;
 var sum = a - b * result;
-console.log(sum);
+// console.log(sum);
 
 if (sum > 0) {
-  console.log("The result is positive.");
+  //   console.log("The result is positive.");
 } else if (sum < 0) {
-  console.log("The result is negative.");
+  //   console.log("The result is negative.");
 } else {
-  console.log("The result is zero.");
+  //   console.log("The result is zero.");
 }
+
+// objects and arrays
+var person = {
+  name: "John",
+  age: 30,
+  hobbies: ["reading", "traveling", "cooking"],
+};
+// console.log(person.name);
+// console.log(person.age);
+// console.log(person.hobbies);
+
+if (person.name.length > 8) {
+  //   console.log(person.name + " is an adult.");
+} else {
+  //   console.log(person.name + " is a minor.");
+}
+
+var array = [1, 2, 3, 4, 5];
+//array.push(6, 7, 8, 9, 10);
+// console.log(array);
+//array.pop();
+array.shift();
+//array.unshift(0, 1);
+console.log(array);
