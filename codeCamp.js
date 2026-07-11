ee0dd876bb9209c8a358a673b032ad28daacabf9
Surma Begam <sbtisha2001@gmail.com>
2026-07-12 01:10:42 +0600

    add the codecamp2

--- codeCamp.js
+++ codeCamp.js
@@ -25,3 +25,25 @@ console.log(
     botLocation +
     ".",
 );
+
+let adjective = "brave";
+let noun = "dragon";
+let verb = "angry";
+let place = "cave";
+let adjective2 = "fiery";
+let noun2 = "pizza";
+
+let firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log("First story: " + firstStory);
+
+adjective = "tiny";
+noun = "rabbit";
+verb = "excited";
+place = "forest";
+adjective2 = "sparkly";
+noun2 = "carrots";
+
+let secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log("Second story: " + secondStory);
