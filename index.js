8ec9c27962691edb9ec3167dff79909460f64bf9
Surma Begam <sbtisha2001@gmail.com>
2026-07-16 00:24:36 +0600

    add count

--- index.js
+++ index.js
@@ -1,62 +1,77 @@
 var name = "World";
-console.log(name);
-console.log(typeof name);
+// console.log(name);
+// console.log(typeof name);
 var age = 30;
-console.log(age);
-console.log(typeof age);
+// console.log(age);
+// console.log(typeof age);
 
 var isStudent = true;
-console.log(isStudent);
-console.log(typeof isStudent);
+// console.log(isStudent);
+// console.log(typeof isStudent);
 
 var hobbies = ["Reading", "Traveling", "Cooking"];
-console.log(hobbies);
-console.log(typeof hobbies);
+// console.log(hobbies);
+// console.log(typeof hobbies);
 
 var person = {
   name: "Alice",
   age: 25,
 };
-console.log(person);
-console.log(typeof person);
+// console.log(person);
+// console.log(typeof person);
 
 // Undefined variable
 var undefinedVariable;
-console.log(undefinedVariable);
-console.log(typeof undefinedVariable);
+// console.log(undefinedVariable);
+// console.log(typeof undefinedVariable);
 
 // Null variable
 var sohel = null;
-console.log(sohel);
-console.log(typeof sohel);
+//console.log(sohel);
+//console.log(typeof sohel);
 
 // Symbol variable
 var uniqueId = Symbol("id");
-console.log(uniqueId);
-console.log(typeof uniqueId);
+//console.log(uniqueId);
+//console.log(typeof uniqueId);
 
 // BigInt variable
 var bigNumber = 123456789012345678901234567890n;
-console.log(bigNumber);
-console.log(typeof bigNumber);
+//console.log(bigNumber);
+//console.log(typeof bigNumber);
 // parseInt and parseFloat
 var num1 = "42";
 var num2 = "3.14";
-console.log(parseInt(num1));
-console.log(parseFloat(num2));
+// console.log(parseInt(num1));
+// console.log(parseFloat(num2));
 
 // isNaN and isFinite
 var value1 = "abc";
 var value2 = 10;
-console.log(isNaN(value1));
-console.log(isFinite(value2));
+// /console.log(isNaN(value1));
+// console.log(isFinite(value2));
 // toString and valueOf
 var num = 123;
-console.log(num.toString());
-console.log(num.valueOf());
-console.log(typeof num);
+// console.log(num.toString());
+//.log(num.valueOf());
+//console.log(typeof num);
 
 // String concatenation
 var greeting = "Hello, " + name + "!";
-console.log(greeting);
-console.log(typeof greeting);
+// console.log(greeting);
+// console.log(typeof greeting);
+
+//session;
+const money = 600;
+
+if (money >= 5500) {
+  // console.log("You can afford the purchase.");
+} else {
+  // console.log("I cannot afford the purchase.");
+}
+
+let count = 30;
+for (let i = 1; i <= count; i++) {
+  i += 2;
+  console.log(i + " is an odd number.");
+}
