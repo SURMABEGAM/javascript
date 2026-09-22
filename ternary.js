efa7e32b5048a92e2d3170e76a39799d61493625
Surma Begam <sbtisha2001@gmail.com>
2026-09-23 03:44:05 +0600

    ternary

---
+++ ternary.js
@@ -0,0 +1,23 @@
+//condition ? true হলে যা হবে : false হলে যা হবে;
+// problem 1
+let age = 20;
+age >= 18 ? console.log("Eligible") : console.log("Not Eligible");
+
+// problem 2
+let number = 10;
+number % 2 === 0 ? console.log("Even") : console.log("Add");
+
+//problem 3
+let temperature = 35;
+
+temperature >= 30 ? console.log("Hot") : console.log("Normal");
+
+// problem4
+let marks = 75;
+
+marks >= 40 ? console.log("Pass") : console.log("Fail");
+
+// problem 5
+let salary = 35000;
+
+salary >= 30000 ? console.log("High Salary") : console.log("Low Salary");
