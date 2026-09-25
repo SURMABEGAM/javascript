9f3e4cd6375ddb378e63f5ce557f2e3bcb8fa845
Surma Begam <sbtisha2001@gmail.com>
2026-09-26 01:16:47 +0600

    ternary6

--- ternary.js
+++ ternary.js
@@ -1,11 +1,11 @@
 //condition ? true হলে যা হবে : false হলে যা হবে;
 // problem 1
-let age = 20;
-age >= 18 ? console.log("Eligible") : console.log("Not Eligible");
+// let age = 20;
+// age >= 18 ? console.log("Eligible") : console.log("Not Eligible");
 
 // problem 2
-let number = 10;
-number % 2 === 0 ? console.log("Even") : console.log("Add");
+// let number = 10;
+// number % 2 === 0 ? console.log("Even") : console.log("Odd");
 
 //problem 3
 let temperature = 35;
@@ -21,3 +21,18 @@ marks >= 40 ? console.log("Pass") : console.log("Fail");
 let salary = 35000;
 
 salary >= 30000 ? console.log("High Salary") : console.log("Low Salary");
+
+// problem:6
+let age = 22;
+age >= 18 ? "Can Drive" : "Cannot Drive";
+
+// problem :7
+let number = 17;
+number % 2 === 0 ? "Even Number" : "Odd Number";
+
+let username = "admin";
+let password = "12345";
+
+username === "admin" && password === "12345"
+  ? "Login Successful"
+  : "Invalid Username or Password";
